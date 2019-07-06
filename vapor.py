dde85a869bed8546ce94f5deca7183504110a7aa
Felipe Custodio <felipe.crochi@gmail.com>
2019-07-05 21:25:35 -0300

    :palm_tree: Better logging, fix file paths

--- vapor.py
+++ vapor.py
@@ -33,20 +33,12 @@ import urllib.parse
 import youtube_dl
 
 # logging
-timestr = time.strftime("%Y%m%d-")
-logdir = os.path.join(os.getcwd(), 'logs')
-logfile = os.path.join(logdir, (timestr + "requests.log"))
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(message)s",
-    handlers=[
-        logging.FileHandler(logfile),
-        logging.StreamHandler()
-    ])
+import logzero
+from logzero import logger
+from logzero import setup_logger
 
-# @felup.io (bot admin)
-LIST_OF_ADMINS = [71491472]
+logzero.logfile("log.log", maxBytes=1e6, backupCount=5)
+stats = setup_logger(name="stats", logfile="requests.log", level=logging.INFO, maxBytes=1e6, backupCount=5)
 
 # vaporwave parameters
 fx = (
@@ -106,19 +98,26 @@ def vapor(query, bot, request_id, chat_id):
         }],
     }
 
+    logger.info("[" + str(request_id) + "] " + "Got request!")
+    logger.debug("[" + str(request_id) + "] " + str(query) + " " + str(chat_id))
+
     # prepare audio files paths (for all systems)
-    root = os.getcwd()
-    original_path = os.path.join(root, (str(request_id) + ".wav"))
-    chorus_path = os.path.join(root, (str(request_id) + "_chorus.wav"))
-    vapor_path = os.path.join(root, (str(request_id) + "_vapor.wav"))
+    logger.info("[" + str(request_id) + "] " + "Preparing audio paths...")
+    original_path = str(request_id) + ".wav"
+    chorus_path = str(request_id) + "_chorus.wav"
+    vapor_path = str(request_id) + "_vapor.wav"
+    logger.debug("[" + str(request_id) + "] " + " " + str(original_path) + " " + str(chorus_path) + " " + str(vapor_path))
 
-    if (len(query) < 1):
-        raise ValueError('No query.')
-
-    bot.send_message(chat_id=chat_id, text=working_str)
+    logger.info("[" + str(request_id) + "] " + "Sending 'Working' message to " + str(chat_id) + '...')
+    try:
+        bot.send_message(chat_id=chat_id, text=working_str)
+    except Exception as e:
+        logger.error("[" + str(request_id) + "] " + e)
+        raise ValueError('Could not send message to user ' + str(chat_id))
 
     # check if query is youtube url
     if not query.lower().startswith((youtube_urls)):
+        logger.info("[" + str(request_id) + "] " + "Searching for YouTube videos...")
         # search for youtube videos matching query
         query_string = urllib.parse.urlencode({"search_query" : query})
         html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
@@ -126,51 +125,60 @@ def vapor(query, bot, request_id, chat_id):
         info = False
 
         # find video that fits max duration
+        logger.info("[" + str(request_id) + "] " + "Get video information...")
         for url in search_results:
             # check for video duration
             try:
                 info = youtube_dl.YoutubeDL(ydl_opts).extract_info(url,download = False)
-            except:
+            except Exception as e:
+                logger.error("[" + str(request_id) + "] " + e)
                 raise ValueError('Could not get information about video.')
             full_title = info['title']
             if (info['duration'] < MAX_DURATION and info['duration'] >= 5):
                 # get first video that fits the limit duration
+                logger.debug("[" + str(request_id) + "] " + "Got video: " + str(full_title))
                 break
         
         # if we ran out of urls, return error
         if (not info):
-            raise ValueError('Could not find video.')
+            raise ValueError('Could not find a video.')
 
     # query was a youtube link
     else:
-        url = query    
+        logger.info("[" + str(request_id) + "] " + "Query was a YouTube URL.")
+        url = query
         info = youtube_dl.YoutubeDL(ydl_opts).extract_info(url,download = False)
         # check if video fits limit duration
         if (info['duration'] < 5 or info['duration'] > MAX_DURATION):
-            raise ValueError('Video too short!')
+            raise ValueError('Video is too short. Need 5 seconds or more.')
 
     # cleanup title
     title = (re.sub(r'\W+', '', info['title']))[:15]
     title = str((title.encode('ascii',errors='ignore')).decode())
 
     # check if cached audio exists
-    vapor_path = Path(title + "_vapor.wav")
-    if vapor_path.is_file():
+    logger.info("[" + str(request_id) + "] " + "Checking if cached audio exists...")
+    vapor_path = title + "_vapor.wav"
+    if Path(vapor_path).is_file():
         vapor_path = title + "_vapor.wav"
         try:
             bot.send_audio(chat_id=chat_id, audio=open(vapor_path, 'rb'))
-        except:
+        except Exception as e:
+            logger.error("[" + str(request_id) + "] " + e)
             raise ValueError('Failed to send audio.')
         return 
 
     # download video and extract audio
+    logger.info("[" + str(request_id) + "] " + "Downloading video...")
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             ydl.download([url])
-        except:
+        except Exception as e:
+            logger.error("[" + str(request_id) + "] " + e)
             raise ValueError('Could not download ' + str(full_title) + '.')
 
     # find and extract music chorus
+    logger.info("[" + str(request_id) + "] " + "Searching for chorus...")
     chorus = False
     chorus_duration = 15 # anything bigger would consume too much memory
     while (not chorus and chorus_duration > 0):
@@ -178,44 +186,58 @@ def vapor(query, bot, request_id, chat_id):
         chorus_duration -= 5
     
     if (not chorus):
+        logger.info("[" + str(request_id) + "] " + "Could not find chorus, using first segment...")
         # could not find chorus, use first seconds instead
         chorus_duration = 15 # reset durations
         try:
             song = AudioSegment.from_wav(original_path)
-        except:
-            raise ValueError('Failed to get audio segment.')
+        except Exception as e:
+            logger.error("[" + str(request_id) + "] " + e)
+            raise ValueError('Failed to get chorus audio segment.')
         
         # get the smallest possible segment
         # video is already guaranteed to be equal or greater than 5 seconds
         while (chorus_duration > info['duration']):
             chorus_duration -= 5
 
-        seconds = chorus_duration * 1000
-        first_seconds = song[:seconds]
-        first_seconds.export(chorus_path, format="wav")
+        try:
+            seconds = chorus_duration * 1000
+            first_seconds = song[:seconds]
+            first_seconds.export(chorus_path, format="wav")
+        except Exception as e:
+            logger.error("[" + str(request_id) + "] " + e)
+            raise ValueError('Failed to export chorus audio segment.')
 
     # make it vaporwave (python wrapper for sox)
-    vapor_path = title + "_vapor.wav"
+    vapor_path = str(title) + "_vapor.wav"
     
-    infile = repr(chorus_path)
-    outfile = repr(vapor_path)
+    infile = str(chorus_path)
+    outfile = str(vapor_path)
 
+    logger.debug("[" + str(request_id) + "] " + str(infile) + " " + str(outfile))
     try:
+        logger.info("[" + str(request_id) + "] " + "Applying Vaporwave SFX...")
         fx(infile, outfile)
+    except Exception as e:
+        logger.error("[" + str(request_id) + "] " + e)
+        raise ValueError('Failed to apply Vaporwave SFX.')
     except:
-        raise ValueError('Failed to apply Vaporwave FX.')
+        logger.error("[" + str(request_id) + "] " + "Unexpected error:", sys.exc_info()[0])
 
     # send audio to user
+    logger.info("[" + str(request_id) + "] " + 'Sending final audio to ' + str(chat_id) + '...')
     try:
         bot.send_audio(chat_id=chat_id, audio=open(vapor_path, 'rb'))
-    except:
+    except Exception as e:
+        logger.error("[" + str(request_id) + "] " + e)
         raise ValueError('Failed to send audio.')
 
     # cleanup
     try:
         os.remove(original_path)
         os.remove(chorus_path)
-    except OSError:
+    except OSError as e:
+        logger.error("[" + str(request_id) + "] " + e)
         pass
 
 
@@ -234,7 +256,7 @@ def vapor_command(bot, update):
     request_id = update.message.message_id
     username = str(bytes(str(update.message.from_user.username), 'utf-8').decode('utf-8', 'ignore'))
    
-    request_text = update.message.text.replace('/vapor','')
+    request_text = update.message.text.replace('/vapor ','')
     request_text = str(bytes(str(request_text), 'utf-8').decode('utf-8', 'ignore'))
 
     chat_id = update.message.chat_id
@@ -243,16 +265,15 @@ def vapor_command(bot, update):
     try:
         start = timer()
         vapor(request_text, bot, request_id, chat_id)
-    except ValueError:
+    except ValueError as e:
+        logger.error("[" + str(request_id) + "] " + e)
         status = "failed"
         bot.send_message(chat_id=chat_id, text=error_str)
     finally:
         end = timer()
         elapsed = str(end - start)
-        try:
-            logger.info("{},{},{},{},{},{}".format(request_date, str(request_id), str(username), str(request_text), str(status), str(elapsed)))
-        except:
-            pass
+        logger.info("[" + str(request_id) + "] " + "Finished.")
+        stats.info("{},{},{},{},{},{}".format(request_date, str(request_id), str(username), str(request_text), str(status), str(elapsed)))
 
 
 @run_async
@@ -262,11 +283,13 @@ def unknown_command(bot, update):
 
 def main():
     # set env variables
+    logger.info("VIRTUAL DREAMS TURNING ON...")
     load_dotenv()
     BOT_TOKEN = os.getenv("TOKEN")
     HEROKU_NAME = "virtualdreamsbot"
     PORT = int(os.environ.get('PORT', '8443'))
 
+    logger.info("Dispatching workers...")
     updater = Updater(token=BOT_TOKEN, workers=2)
     dispatcher = updater.dispatcher
 
@@ -277,31 +300,32 @@ def main():
     unknown_handler = MessageHandler(Filters.command, unknown_command)
 
     # start bot handlers
+    logger.info("Starting handlers...")
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(vapor_handler)
     dispatcher.add_handler(unknown_handler)
 
     # move working directory to cache
+    logger.info("Setting working directory...")
     if not os.path.exists("cache") and not (str(os.path.basename(os.getcwd())) == "cache"):
         try:
             os.makedirs("cache")
             os.chdir("cache")
-        except:
-            raise
+        except OSError as e:
+            logger.error(e)
     else:
         os.chdir("cache")
 
-    logger.info("date,request_id,username,request_text,success,time_elapsed")
-
     # heroku webhook
-    updater.start_webhook(listen="0.0.0.0",
+    # updater.start_webhook(listen="0.0.0.0",
                         #   port=PORT,
                         #   url_path=BOT_TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(HEROKU_NAME, BOT_TOKEN))
+    # updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(HEROKU_NAME, BOT_TOKEN))
 
     # local hosting
-    # updater.start_polling()
+    logger.info("VIRTUAL DREAMS ONLINE")
+    updater.start_polling()
     updater.idle()
 
 
